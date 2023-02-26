03d8e80b74790d69f1a6e413fc743ce15844ee0a
Sahra Di Bernardi <sah.dibernardi@gmail.com>
2023-02-26 19:13:16 -0300

    add: simple RESTful API that works on localhost

--- back-end/soccer-team-manager/src/app.js
+++ back-end/soccer-team-manager/src/app.js
@@ -2,6 +2,58 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+const teams = [
+    {
+      id: 1,
+      name: 'São Paulo Futebol Clube',
+      initials: 'SPF',
+    },
+    {
+      id: 2,
+      name: 'Clube Atlético Mineiro',
+      initials: 'CAM',
+    },
+  ];
+
+app.get('/', (req, res) => res.status(200).json(
+    { message: 'Welcome to the Soccer Team Management API' },
+    ));
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+// add data to the API
+app.use(express.json());
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+// admits edit on a specific team name and initials
+app.put('/teams/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, initials } = req.body;
+  
+    const updateTeam = teams.find((team) => team.id === Number(id));
+  
+    if (!updateTeam) {
+      res.status(404).json({ message: 'Team not found' });
+    }
+  
+    updateTeam.name = name;
+    updateTeam.initials = initials;
+    res.status(200).json({ updateTeam });
+  });
+
+// delete an specific team, based on it's id
+app.delete('/teams/:id', (req, res) => {
+    const { id } = req.params;
+    const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+    teams.splice(arrayPosition, 1);
+  
+    res.status(200).end();
+  });
 
 module.exports = app;
